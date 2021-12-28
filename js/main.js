fa04e1dd81123247e2636c099da5cf33ec9ab65f
Adrian Cortes <ls-adrian@hotmail.com>
2021-12-27 22:17:29 -0500

    We add the DOM events and the addProduct method to the UI class

--- js/main.js
+++ js/main.js
@@ -7,8 +7,20 @@ class Product {
 }
 
 class UI {
-   addProduct(){
+   addProduct(product){
+      const productList = document.querySelector('#product-list');
+      const element = document.createElement('div');
 
+      element.innerHTML = `
+         <div class="card text-center mb-3">
+            <div class="card-body">
+               <strong>Product Name</strong>: ${product.name}
+               <strong>Product Price</strong>: ${product.price}
+               <strong>Product Year</strong>: ${product.year}
+            </div>
+         </div>
+      `
+      productList.appendChild(element);
    }
 
    deleteProduct(){
@@ -19,3 +31,17 @@ class UI {
 
    }
 }
+// DOM Events
+document.querySelector('#product__form').addEventListener('submit', (e)=>{
+   const name = document.querySelector('#name').value;
+   const price = document.querySelector('#price').value;
+   const year = document.querySelector('#year').value;
+
+   const product = new Product(name, price, year);
+   const ui= new UI();
+
+   ui.addProduct(product)
+
+   e.preventDefault()
+})
+
